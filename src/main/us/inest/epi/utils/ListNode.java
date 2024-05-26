7eb92e70ef9d4547021d199bd1358eaa50fa9768
hieu-van-nguyen <hieuacctforgcp@gmail.com>
2024-05-26 12:23:37 -0500

    MergeSortedLists

---
+++ src/main/us/inest/epi/utils/ListNode.java
@@ -0,0 +1,11 @@
+package us.inest.epi.utils;
+
+public class ListNode<T> {
+    public T data;
+    public ListNode<T> next;
+
+    public ListNode(T data, ListNode<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+}
--- src/main/us/inest/epi/utils/ListUtil.java
+++ src/main/us/inest/epi/utils/ListUtil.java
@@ -16,4 +16,27 @@ public class ListUtil {
             displayList(row);
         }
     }
+
+    public static ListNode<Integer> initLinkedList(int[] input) {
+        ListNode<Integer> dummyHead = new ListNode<>(0, null);
+        ListNode<Integer> current = dummyHead;
+        for (int num : input) {
+            current.next = new ListNode<>(num, null);
+            current = current.next;
+        }
+        return dummyHead.next;
+    }
+
+    public static void displayLinkedList(ListNode<Integer> head) {
+        ListNode<Integer> current = head;
+        while (current != null) {
+            if (current.next == null) {
+                //Last node
+                System.out.println(current.data);
+            } else {
+                System.out.print(current.data + " ");
+            }
+            current = current.next;
+        }
+    }
 }
